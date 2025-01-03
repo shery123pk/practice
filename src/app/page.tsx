c91e2ae6e8fab19c79cda69ad2a1a3c25db847de
shery123pk <madni.kali@gmail.com>
2025-01-03 10:20:53 +0500

    4th comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>this is 3rd comment</h1>
+    <div className="flex justify-between">
+      <h1 className="font-bold text-[24px]">logo</h1>
+      <ul className="px-5">
+        <li >
+          Home
+          About
+          Contact Us
+        </li>
+      </ul>
+      
     </div>
   );
 }
